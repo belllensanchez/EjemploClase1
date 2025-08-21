57e22abac917160917c8460d8864599184c1c868
belllensanchez <belu138@hotmail.com>
2025-08-21 01:21:32 -0300

    Agregado metodo GetPersona

---
+++ EjemploClase1/Controllers/PersonaController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EjemploClase1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+       
+
+        
+        
+
+       // POST api/<PersonaController>
+        [HttpPost]
+        public List< Persona> GetPersona(Persona persona)
+        {
+            List<Persona> lista =new List<Persona>();
+
+            for (int  i = 0; i <5;i ++)
+            {
+                lista.Add(persona);
+            }
+            return lista;
+
+
+
+        }
+
+
+       
+    }
+}
